de04c9cb841e3f9e4815e6cadb8f5a4713944383
Abdo Taleb <44838067+AbdoTaleb@users.noreply.github.com>
2024-12-19 11:49:08 +0100

    updating my portfolio

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
-import './Hero.css'
-import profile_img from '../../assets/profile_img.svg'
-import AnchorLink from 'react-anchor-link-smooth-scroll';
-import me from '../../assets/me.jpeg'
+import React from "react";
+import "./Hero.css";
+import profile_img from "../../assets/profile_img.svg";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import me from "../../assets/me.jpeg";
 
 const Hero = () => {
   return (
-    <div id='home' className="hero">
-        <img src={me} alt="" />
-        <h1><span>I’m Abdo Taleb,</span> a dedicated and experienced Tech Lead based in Karlskrona, Sweden.</h1>
-        <p>With a strong background in software engineering</p>
-        <div className="hero-action">
-            <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect With Me</AnchorLink></div>
-            <div className="hero-resume">My resume</div>
+    <div id="home" className="hero">
+      <img src={me} alt="" />
+      <h1>
+        <span>Hello, This is Abdo Taleb,</span> I'm a Software Developer. in
+        Malmö, Sweden.
+      </h1>
+      <p>
+        Previously, I worked at Telenor in Karlskrona as a Tech Lead and
+        Developer.
+      </p>
+      <div className="hero-action">
+        <div className="hero-connect">
+          <AnchorLink className="anchor-link" offset={50} href="#contact">
+            Connect With Me
+          </AnchorLink>
         </div>
+        <div className="hero-resume">My resume</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
