3eee5ed438242d2f88e2fc2ebb9088c0dad1e728
Abdo Taleb <44838067+AbdoTaleb@users.noreply.github.com>
2024-12-19 11:50:56 +0100

    updating my location

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import './Contact.css'
-import theme_pattern from '../../assets/underline.svg'
-import mail_icon from '../../assets/mail_icon.svg'
-import location_icon from '../../assets/location_icon.svg'
-import call_icon from '../../assets/call_icon.svg'
+import React from "react";
+import "./Contact.css";
+import theme_pattern from "../../assets/underline.svg";
+import mail_icon from "../../assets/mail_icon.svg";
+import location_icon from "../../assets/location_icon.svg";
+import call_icon from "../../assets/call_icon.svg";
 
 const Contact = () => {
-
   const onSubmit = async (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
@@ -20,9 +19,9 @@ const Contact = () => {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Accept: "application/json"
+        Accept: "application/json",
       },
-      body: json
+      body: json,
     }).then((res) => res.json());
 
     if (res.success) {
@@ -31,39 +30,51 @@ const Contact = () => {
   };
 
   return (
-    <div id='contact' className='contact'>
+    <div id="contact" className="contact">
       <div className="contact-title">
         <h1>Get in touch</h1>
         <img src={theme_pattern} alt="" />
       </div>
       <div className="contact-section">
         <div className="contact-left">
-            <h1>Let's talk</h1>
-            <p>when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
-            <div className="contact-details">
-                <div className="contact-detail">
-                    <img src={mail_icon} alt="" /><p>abdo.taleb1@hotmail.com</p>
-                </div>
-                <div className="contact-detail">
-                    <img src={call_icon} alt="" /><p>072 053 72 84</p>
-                </div>
-                <div className="contact-detail">
-                <img src={location_icon} alt="" /><p>Karlskrona, Sweden</p>
-                </div>
-            </div>       
+          <h1>Let's talk</h1>
+          <p>
+            when an unknown printer took a galley of type and scrambled it to
+            make a type specimen book. It has survived not only five centuries
+          </p>
+          <div className="contact-details">
+            <div className="contact-detail">
+              <img src={mail_icon} alt="" />
+              <p>abdo.taleb1@hotmail.com</p>
+            </div>
+            <div className="contact-detail">
+              <img src={call_icon} alt="" />
+              <p>072 053 72 84</p>
+            </div>
+            <div className="contact-detail">
+              <img src={location_icon} alt="" />
+              <p>Malmö, Sweden</p>
+            </div>
+          </div>
         </div>
         <form onSubmit={onSubmit} className="contact-right">
-            <label htmlFor="">Your Name</label>
-            <input type="text" placeholder='Enter your name' name='name'/>
-            <label htmlFor="">Your Email</label>
-            <input type="email" placeholder='Enter your email' name='email'/>
-            <label htmlFor="">Write your message here</label>
-            <textarea name="message" rows="8" placeholder='Enter your message here'></textarea>
-            <button type='submit' className="contact-submit">Submit now</button>
+          <label htmlFor="">Your Name</label>
+          <input type="text" placeholder="Enter your name" name="name" />
+          <label htmlFor="">Your Email</label>
+          <input type="email" placeholder="Enter your email" name="email" />
+          <label htmlFor="">Write your message here</label>
+          <textarea
+            name="message"
+            rows="8"
+            placeholder="Enter your message here"
+          ></textarea>
+          <button type="submit" className="contact-submit">
+            Submit now
+          </button>
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
